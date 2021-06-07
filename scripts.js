7292de9922281d3ad2115e14f1025b7114137733
Rob Hagen <57631113+robhagen@users.noreply.github.com>
2021-06-07 12:32:47 +0200

    Added functional logic

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("button_userinput");
+
+document.getElementById("card_image").addEventListener("click", function() {
+    console.log("Hallo!");
+  });
+
+// document.getElementById("card_image").src = ("https://robohash.org/" + userinput)
+//let userinput = document.getElementById("userinput").value
