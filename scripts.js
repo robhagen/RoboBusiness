b679ed535eb433fe7c2c77d00e4d89a367b76a87
Rob Hagen <57631113+robhagen@users.noreply.github.com>
2021-06-08 14:32:45 +0200

    Added functionality and API connection

--- scripts.js
+++ scripts.js
@@ -1,8 +1,22 @@
 let button = document.getElementById("button_userinput");
 
-document.getElementById("card_image").addEventListener("click", function() {
-    console.log("Hallo!");
-  });
+/* Gets the current date and time and coverts to a robot-headshot*/
+let currentDate = new Date();
+document.getElementById("cardmaker_image").src = ("https://robohash.org/" + currentDate + "?set=set3")
 
-// document.getElementById("card_image").src = ("https://robohash.org/" + userinput)
-//let userinput = document.getElementById("userinput").value
+/* When the button is clicked this function takes whatever the user had input and converts it to a robot image*/
+/* Then it displays the entered name next to the image as an employee */
+/* Lastly it moves the page to the anchor displaying the businesscard */
+button.onclick = function() {
+    let userinput = document.getElementById("userinput").value
+    document.getElementById("card_image").src = ("https://robohash.org/" + userinput + "?size=200x200")
+    document.getElementById("username").innerHTML = (userinput);
+    goToAnchor();
+};
+
+/* This function simply moves the viewport to the anchor displaying the businesscard, is called at .onclick */
+function goToAnchor(anchor) {
+    document.location = '#businesscard';
+    return false;
+  }
+
