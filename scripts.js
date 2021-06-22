9c907f24286b7f9d1208facb95a85f5a4e3e8775
Rob Hagen <57631113+robhagen@users.noreply.github.com>
2021-06-22 20:51:11 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,23 +1,28 @@
-let button = document.getElementById("button_userinput");
+let button_userInput = document.getElementById("button_userinput");
+let button_backToTop = document.getElementById("button_backToTop");
 
-/* Gets the current date and time and coverts to a robot-headshot*/
+/* Gets the current date and time and coverts to a robot-headshot */
 let currentDate = new Date();
 document.getElementById("cardmaker_image").src = ("https://robohash.org/" + currentDate + "?set=set3")
 
-/* When the button is clicked this function takes whatever the user had input and converts it to a robot image*/
+/* When the button is clicked this function takes whatever the user had input and converts it to a robot image */
 /* Then it displays the entered name next to the image as an employee */
 /* Lastly it moves the page to the anchor displaying the businesscard */
-button.onclick = function() {
+button_userInput.onclick = function() {
     let userinput = document.getElementById("userinput").value
     document.getElementById("card_image").src = ("https://robohash.org/" + userinput + "?size=200x200")
     document.getElementById("username").innerHTML = (userinput);
-    goToAnchor();
     generateTelephoneNumber();
+    goToAnchor("#businesscard");
 };
 
-/* This function simply moves the viewport to the anchor displaying the businesscard, is called at .onclick */
+button_backToTop.onclick = function() {
+  goToAnchor("#home");
+};
+
+/* This function simply moves the viewport to the given anchor, is called at .onclick */
 function goToAnchor(anchor) {
-    document.location = '#businesscard';
+    document.location = anchor;
     return false;
   }
 
